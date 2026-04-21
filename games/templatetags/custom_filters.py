d11f9b5cb153cd92bba0492ec42449daca8b15c5
Luan Gustavo <gluan1213@gmail.com>
2026-04-21 17:43:58 -0300

    feat: create a markdown render method to display the game description correctly

--- games/templatetags/custom_filters.py
+++ games/templatetags/custom_filters.py
@@ -1,6 +1,9 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
+
 
 @register.filter
 def pretty_username(value):
@@ -9,3 +12,8 @@ def pretty_username(value):
 
     normalized = str(value).replace("_", " ").replace("-", " ")
     return " ".join(normalized.split()).title()
+
+
+@register.filter
+def render_markdown(text):
+    return mark_safe(markdown.markdown(text))
